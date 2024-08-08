e6e45ee3025edac934d52470813b43b3816c2719
Maksymilian Szkorupa <maksszkorupa@interia.pl>
2024-08-08 21:48:23 +0200

    MySlf >Executive
    --------------------
    Patchlog 1.0
    
    [+] Cala strona
    
    --------------------

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Myslf</title>
+    <link href="css-own/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container-fluid d-flex justify-content-center align-items-center">
+        <div class="myslf-box">
+            <div class="myslf-box-upper d-flex justify-content-center align-items-center">
+                <div class="title p-0"><p class="myslf-box-title">MYSLF</p> <p class="myslf-box-desc">>Executive</p></div>
+            </div>
+            <div class="myslf-box-bottom justify-content-center d-flex align-items-center">
+                <ul class="p-1 m-0">
+                    <a href="https://www.youtube.com/@tojasolan">
+                        <div class="myslf-item d-flex align-items-center p-3 mt-3 mb-3">
+                            <div class="logo-item d-flex justify-content-center align-items-center">
+                                <img src="img/youtube.webp" class="square">
+                            </div>
+                            <div class="empty "></div>
+                            <div class="item-text">
+                                Youtube
+                            </div>
+                        </div>
+                    </a>
+                    <a href="https://www.twitch.tv/grendy">
+                        <div class="myslf-item d-flex align-items-center p-3 mt-3 mb-3">
+                            <div class="logo-item d-flex justify-content-center align-items-center">
+                                <img src="img/twitch.png" class="square">
+                            </div>
+                            <div class="empty "></div>
+                            <div class="item-text">
+                                Twitch
+                            </div>
+                        </div>
+                    </a>
+                    <a href="https://www.spotify.com/signout">
+                        <div class="myslf-item d-flex align-items-center p-3 mt-3 mb-3">
+                            <div class="logo-item d-flex justify-content-center align-items-center">
+                                <img src="img/spotify.webp" class="square">
+                            </div>
+                            <div class="empty "></div>
+                            <div class="item-text">
+                                Spotify
+                            </div>
+                        </div>
+                    </a>
+                    <a href="https://www.facebook.com/MaksymilianSzkorupa">
+                        <div class="myslf-item d-flex align-items-center p-3 mt-3 mb-3">
+                            <div class="logo-item d-flex justify-content-center align-items-center">
+                                <img src="img/messenger.png" class="square">
+                            </div>
+                            <div class="empty"></div>
+                            <div class="item-text">
+                                Facebook
+                            </div>
+                        </div>
+                    </a>
+                    <a href="https://www.github.com/szkorupa">
+                        <div class="myslf-item d-flex align-items-center p-3 mt-3 mb-3">
+                            <div class="logo-item d-flex justify-content-center align-items-center">
+                                <img src="img/git.svg" class="square">
+                            </div>
+                            <div class="empty"></div>
+                            <div class="item-text">
+                                Github
+                            </div>
+                        </div>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
